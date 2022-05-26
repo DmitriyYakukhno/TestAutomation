3b32e46c867bdc0780ad475cfdb5dab03bba680c
unknown <DmytroYakukhno@gmail.com>
2022-05-26 17:28:07 +0300

    check

--- src/test/java/Lessons/MainClass.java
+++ src/test/java/Lessons/MainClass.java
@@ -2,20 +2,31 @@ package Lessons;
 
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Array;
+
 public class MainClass {
 
-//    @Test
-//    public void tester(){
-//        int aDiff = Math.(4 - 10);
-//        System.out.println(aDiff);
+    @Test
+    public void tester() {
+        int factorial = 4;
+        int result = 1;
+        Math.
+        if (factorial < 2) {
+            result = 1;
+        }
+        for (int i = factorial; i > 0; i--) {
+            result = result * i;
+        }
+        System.out.println(result);
+    }
+
+
+//        int[] arr = {2, 3, 4, 7};
+//        System.out.println(arr.length);
+//        for (int i = 1; i < n; i++) {
+//            str = str + str;
 //        }
-//
-//    public String startHi(int a, int b) {
-//
-//
-//    }
-//    @Test
-//    public void doit(){
-//        startHi(6,7);
-//    }
+//        System.out.println(str);
+
+
 }
